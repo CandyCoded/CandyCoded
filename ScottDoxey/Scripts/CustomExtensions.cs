ab75f4856066a96ec90db1e73d6c62d03a1aaa55
Scott Doxey <neogeek0101@gmail.com>
2017-12-17 23:47:33 -0500

    Added comment blocks to custom extensions.

--- ScottDoxey/Scripts/CustomExtensions.cs
+++ ScottDoxey/Scripts/CustomExtensions.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public static class CustomExtensions {
 
+    /// <summary>
+    /// Is laeyrName in layerMask? layerName: Layer name to compare against layerMask.
+    /// </summary>
     public static bool Contains(this LayerMask layerMask, string layerName) {
 
         return layerMask.Contains(LayerMask.NameToLayer(layerName));
 
     }
 
+    /// <summary>
+    /// Is layerInt in layerMask? layerInt: Layer number to compare against layerMask.
+    /// </summary>
     public static bool Contains(this LayerMask layerMask, int layerInt) {
 
         return layerMask == (layerMask | (1 << layerInt));
 
     }
 
+    /// <summary>
+    /// Rotates the transform so the forward vector points at target's position. target: Obkect to point towards. direction: Vector specifying the fotward direction.
+    /// </summary>
     public static void LookAt2D(this Transform transform, Transform target, Vector3 direction) {
 
         Vector2 angle = target.position - transform.position;
@@ -24,6 +33,9 @@ public static class CustomExtensions {
 
     }
 
+    /// <summary>
+    /// Rotates the transform so the forward vector points at target's position. target: Obkect to point towards.
+    /// </summary>
     public static void LookAt2D(this Transform transform, Transform target) {
 
         Vector2 angle = target.position - transform.position;
