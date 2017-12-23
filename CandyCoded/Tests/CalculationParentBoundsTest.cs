daccc03f10c8e0f00cad771b4f1e144d99ac3864
Scott Doxey <neogeek0101@gmail.com>
2017-12-23 15:12:54 -0500

    Added test for Calculation.ParentBounds

---
+++ CandyCoded/Tests/CalculationParentBoundsTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CalculationParentBoundsTest {
+
+    [UnityTest]
+    public IEnumerator NewPlayModeTestWithEnumeratorPasses() {
+
+        GameObject parentGameObject = new GameObject("ParentGameObject");
+
+        GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1.transform.position = Vector3.back * 2;
+        cube1.transform.parent = parentGameObject.transform;
+
+        GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube2.transform.position = Vector3.left * 2;
+        cube2.transform.parent = parentGameObject.transform;
+
+        GameObject cube3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube3.transform.position = Vector3.right;
+        cube3.transform.parent = parentGameObject.transform;
+
+        Bounds bounds = CandyCoded.Calculation.ParentBounds(parentGameObject);
+
+        Assert.AreEqual(bounds.center.x, -0.5f);
+        Assert.AreEqual(bounds.center.y, 0);
+        Assert.AreEqual(bounds.center.z, -1.0f);
+
+        Assert.AreEqual(bounds.min.x, -2.5f);
+        Assert.AreEqual(bounds.min.y, -0.5f);
+        Assert.AreEqual(bounds.min.z, -2.5f);
+
+        Assert.AreEqual(bounds.max.x, 1.5f);
+        Assert.AreEqual(bounds.max.y, 0.5f);
+        Assert.AreEqual(bounds.max.z, 0.5f);
+
+        Assert.AreEqual(bounds.size.x, 4.0f);
+        Assert.AreEqual(bounds.size.y, 1.0f);
+        Assert.AreEqual(bounds.size.z, 3.0f);
+
+        yield return null;
+
+    }
+}
