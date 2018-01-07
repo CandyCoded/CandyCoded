06c30f9cc639621eaa4903bdb8eea7d34fdc5616
Scott Doxey <neogeek0101@gmail.com>
2018-01-07 12:30:52 -0500

    Added EnumMask inspector type.

---
+++ CandyCoded/Scripts/CustomEditor/EnumMaskAttribute.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnumMaskAttribute : PropertyAttribute {
+
+    public string name;
+
+    public EnumMaskAttribute() { }
+
+    public EnumMaskAttribute(string name) {
+
+        this.name = name;
+
+    }
+
+}
---
+++ CandyCoded/Scripts/CustomEditor/EnumMaskDrawer.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EnumMaskAttribute))]
+public class EnumMaskDrawer : PropertyDrawer {
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+        Enum enumValue = (Enum) fieldInfo.GetValue(property.serializedObject.targetObject);
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        Enum enumNew = EditorGUI.EnumFlagsField(position, label, enumValue);
+
+        property.intValue = (int) Convert.ChangeType(enumNew, enumValue.GetType());
+
+        EditorGUI.EndProperty();
+
+    }
+
+}
