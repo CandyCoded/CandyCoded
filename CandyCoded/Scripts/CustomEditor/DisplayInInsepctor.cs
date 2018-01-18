8459ff372eaa057754364705fe23ef5fa27315bd
Scott Doxey <neogeek0101@gmail.com>
2018-01-17 23:47:28 -0500

    Added DisplayInInsepctor.

---
+++ CandyCoded/Scripts/CustomEditor/DisplayInInsepctor.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Method)]
+public class DisplayInInsepctorAttribute : PropertyAttribute { }
+
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class DisplayInInsepctorDrawer : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        DrawDefaultInspector();
+
+        MemberInfo[] methods = target.GetType().GetMembers(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+        GUILayout.Space(10);
+
+        foreach (MemberInfo method in methods)
+        {
+
+            if (Attribute.IsDefined(method, typeof(DisplayInInsepctorAttribute)))
+            {
+
+                if (GUILayout.Button(ObjectNames.NicifyVariableName(method.Name)))
+                {
+
+                    ((MethodInfo) method).Invoke(target, null);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
