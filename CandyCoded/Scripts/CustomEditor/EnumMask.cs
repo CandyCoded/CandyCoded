1025785e8d8cff477d8a343d8d15e06c51b697d8
Scott Doxey <neogeek0101@gmail.com>
2018-01-17 20:02:47 -0500

    Combined EnumMask into one file.

---
+++ CandyCoded/Scripts/CustomEditor/EnumMask.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class EnumMaskAttribute : PropertyAttribute
+{
+
+    public string name;
+
+    public EnumMaskAttribute() { }
+
+    public EnumMaskAttribute(string name)
+    {
+
+        this.name = name;
+
+    }
+
+}
+
+[CustomPropertyDrawer(typeof(EnumMaskAttribute))]
+public class EnumMaskDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+
+        Enum enumValue = (Enum) fieldInfo.GetValue(property.serializedObject.targetObject);
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        Enum enumNew = EditorGUI.EnumFlagsField(position, label, enumValue);
+
+        property.intValue = (int) Convert.ChangeType(enumNew, enumValue.GetType());
+
+        EditorGUI.EndProperty();
+
+    }
+
+}
