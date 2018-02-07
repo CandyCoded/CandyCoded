f5883848b7fc30de9afb099917a31f436b1dba92
Scott Doxey <neogeek0101@gmail.com>
2018-02-06 22:15:28 -0500

    Store Unity package in $HOME/cache.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -d "cache" ] ; then
+if [ ! -d "$HOME/cache" ] ; then
 
-    mkdir -p -m 777 cache
+    mkdir -p -m 777 "$HOME/cache"
 
     echo "Downloading Unity"
-    curl --retry 5 -o cache/Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
+    curl --retry 5 -o "$HOME/cache/Unity.pkg" https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
 
 fi
 
 echo "Installing Unity"
-sudo installer -dumplog -package cache/Unity.pkg -target /
+sudo installer -dumplog -package "$HOME/cache/Unity.pkg" -target /
