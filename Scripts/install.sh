b3792c0b4fe8ba13ed3bdc5cf6c8e236e77e8c19
Scott Doxey <neogeek0101@gmail.com>
2018-02-06 21:59:59 -0500

    Added cached files.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "Downloading Unity"
-curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
+if [ ! -d "cache" ] ; then
+
+    mkdir -p -m 777 cache
+
+    echo "Downloading Unity"
+    curl --retry 5 -o cache/Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
+
+fi
 
 echo "Installing Unity"
 sudo installer -dumplog -package Unity.pkg -target /
