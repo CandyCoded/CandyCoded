c1e77e88e74b9446e2b1f7b9fa50fe8acc451d83
Scott Doxey <neogeek0101@gmail.com>
2018-02-07 19:44:36 -0500

    Whitespace.

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile "$(pwd)/unity.log" \
-	-projectPath "$(pwd)/CandyCoded" \
-	-runEditorTests \
-	-editorTestsResultFile "$(pwd)/test.xml"
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -logFile "$(pwd)/unity.log" \
+    -projectPath "$(pwd)/CandyCoded" \
+    -runEditorTests \
+    -editorTestsResultFile "$(pwd)/test.xml"
 
 CODE=$?
 
