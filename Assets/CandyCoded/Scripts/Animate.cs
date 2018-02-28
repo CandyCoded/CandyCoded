b03ed3f8d132c084a2b0484450d7c7d3987175d0
Scott Doxey <neogeek0101@gmail.com>
2018-02-27 22:53:06 -0500

    Added coroutine helper methods.

--- Assets/CandyCoded/Scripts/Animate.cs
+++ Assets/CandyCoded/Scripts/Animate.cs
@@ -9,6 +9,22 @@ namespace CandyCoded
 
         public delegate void AnimationFunc(GameObject gameObject, Vector3AnimationCurve animationCurve, float deltaTime, AnimationData animationData = null);
 
+        public static CandyCoded.AnimationRunner GetAnimationRunner(GameObject gameObject)
+        {
+
+            CandyCoded.AnimationRunner animationRunner = gameObject.GetComponent<CandyCoded.AnimationRunner>();
+
+            if (animationRunner == null)
+            {
+
+                animationRunner = gameObject.AddComponent<CandyCoded.AnimationRunner>();
+
+            }
+
+            return animationRunner;
+
+        }
+
         public static CandyCoded.AnimationData GetAnimationData(GameObject gameObject)
         {
 
@@ -36,13 +52,22 @@ namespace CandyCoded
             while (elapsedTime < maxTime)
             {
 
-                animationFunc(gameObject, animationCurve, elapsedTime);
+                animationFunc(gameObject, animationCurve, elapsedTime, animationData);
 
                 elapsedTime += Time.deltaTime;
 
                 yield return null;
 
             }
+
+        }
+
+        public static Coroutine Position(GameObject gameObject, Vector3AnimationCurve animationCurve)
+        {
+
+            IEnumerator routine = CandyCoded.Animate.Loop(gameObject, animationCurve, CandyCoded.Animate.Position);
+
+            return CandyCoded.Animate.GetAnimationRunner(gameObject).StartCoroutine(routine);
 
         }
 
@@ -55,12 +80,30 @@ namespace CandyCoded
 
         }
 
+        public static Coroutine PositionRelative(GameObject gameObject, Vector3AnimationCurve animationCurve)
+        {
+
+            IEnumerator routine = CandyCoded.Animate.Loop(gameObject, animationCurve, CandyCoded.Animate.PositionRelative);
+
+            return CandyCoded.Animate.GetAnimationRunner(gameObject).StartCoroutine(routine);
+
+        }
+
         public static void PositionRelative(GameObject gameObject, Vector3AnimationCurve animationCurve, float deltaTime, AnimationData animationData = null)
         {
 
             if (animationData == null) animationData = GetAnimationData(gameObject);
 
             gameObject.transform.localPosition = animationData.transformData.position + animationCurve.Evaluate(deltaTime);
+
+        }
+
+        public static Coroutine Rotate(GameObject gameObject, Vector3AnimationCurve animationCurve)
+        {
+
+            IEnumerator routine = CandyCoded.Animate.Loop(gameObject, animationCurve, CandyCoded.Animate.Rotate);
+
+            return CandyCoded.Animate.GetAnimationRunner(gameObject).StartCoroutine(routine);
 
         }
 
@@ -73,12 +116,30 @@ namespace CandyCoded
 
         }
 
+        public static Coroutine Scale(GameObject gameObject, Vector3AnimationCurve animationCurve)
+        {
+
+            IEnumerator routine = CandyCoded.Animate.Loop(gameObject, animationCurve, CandyCoded.Animate.Scale);
+
+            return CandyCoded.Animate.GetAnimationRunner(gameObject).StartCoroutine(routine);
+
+        }
+
         public static void Scale(GameObject gameObject, Vector3AnimationCurve animationCurve, float deltaTime, AnimationData animationData = null)
         {
 
             if (animationData == null) animationData = GetAnimationData(gameObject);
 
             gameObject.transform.localScale = animationCurve.Evaluate(deltaTime);
+
+        }
+
+        public static Coroutine ScaleRelative(GameObject gameObject, Vector3AnimationCurve animationCurve)
+        {
+
+            IEnumerator routine = CandyCoded.Animate.Loop(gameObject, animationCurve, CandyCoded.Animate.ScaleRelative);
+
+            return CandyCoded.Animate.GetAnimationRunner(gameObject).StartCoroutine(routine);
 
         }
 
---
+++ Assets/CandyCoded/Scripts/AnimationRunner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+    public class AnimationRunner : MonoBehaviour
+    {
+
+    }
+
+}
