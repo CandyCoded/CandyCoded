0f2915512270225ecdd110aebfe178061bfdb4b7
Scott Doxey <neogeek0101@gmail.com>
2018-03-04 20:51:51 -0500

    Added AddCoroutine and RemoveCoroutine to runner.

--- Assets/CandyCoded/Scripts/AnimationRunner.cs
+++ Assets/CandyCoded/Scripts/AnimationRunner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,29 @@ namespace CandyCoded
     {
 
         public Dictionary<string, Coroutine> coroutines = new Dictionary<string, Coroutine>();
+
+        public Coroutine AddCoroutine(string coroutineKey, IEnumerator routine)
+        {
+
+            coroutines.Add(coroutineKey, StartCoroutine(routine));
+
+            return coroutines[coroutineKey];
+
+        }
+
+        public void RemoveCoroutine(string coroutineKey)
+        {
+
+            if (coroutines.ContainsKey(coroutineKey))
+            {
+
+                StopCoroutine(coroutines[coroutineKey]);
+
+                coroutines.Remove(coroutineKey);
+
+            }
+
+        }
 
     }
 
