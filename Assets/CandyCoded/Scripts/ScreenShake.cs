5ef6c4914aecc1c9b6fc40eba2579212d248b50f
Scott Doxey <neogeek0101@gmail.com>
2018-03-06 20:55:10 -0500

    Changed to a switch statement.

--- Assets/CandyCoded/Scripts/ScreenShake.cs
+++ Assets/CandyCoded/Scripts/ScreenShake.cs
@@ -36,22 +36,20 @@ namespace CandyCoded
             if (currentDuration > 0)
             {
 
-                if (currentDirection == SCREENSHAKE_DIRECTION.All)
+                switch (currentDirection)
                 {
 
-                    shakePosition = Random.insideUnitCircle * currentIntensity;
+                    case SCREENSHAKE_DIRECTION.All:
+                        shakePosition = Random.insideUnitCircle * currentIntensity;
+                        break;
 
-                }
-                else if (currentDirection == SCREENSHAKE_DIRECTION.Horizontal)
-                {
+                    case SCREENSHAKE_DIRECTION.Horizontal:
+                        shakePosition = new Vector3(Random.Range(-1, 1), 0, 0) * currentIntensity;
+                        break;
 
-                    shakePosition = new Vector3(Random.Range(-1, 1), 0, 0) * currentIntensity;
-
-                }
-                else if (currentDirection == SCREENSHAKE_DIRECTION.Vertical)
-                {
-
-                    shakePosition = new Vector3(0, Random.Range(-1, 1), 0) * currentIntensity;
+                    case SCREENSHAKE_DIRECTION.Vertical:
+                        shakePosition = new Vector3(0, Random.Range(-1, 1), 0) * currentIntensity;
+                        break;
 
                 }
 
