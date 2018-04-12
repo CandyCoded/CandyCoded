85104a1562a00fd96fc67c89ae66c81e0248e417
Scott Doxey <neogeek0101@gmail.com>
2018-04-11 21:56:51 -0400

    Changed print margin.

--- Scripts/build_docs.sh
+++ Scripts/build_docs.sh
@@ -3,5 +3,5 @@
 find Documentation -name "*.md" | while read file; do
     echo "Converting ${file}"
     mkdir -p "Assets/Plugins/CandyCoded/${file%/*}"
-    pandoc -V geometry:margin=1.25in "$file" -o "Assets/Plugins/CandyCoded/${file%.md}.pdf"
+    pandoc -V geometry:margin=1in "$file" -o "Assets/Plugins/CandyCoded/${file%.md}.pdf"
 done
