ae33aa3b3f9c5481df29323b0af107d4497b10a9
Scott Doxey <neogeek0101@gmail.com>
2018-05-30 22:49:19 -0400

    Fixed bash warnings.

--- Scripts/build_docs.sh
+++ Scripts/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find Documentation -name "*.md" | while read file; do
+find Documentation -name "*.md" | while read -r file; do
     echo "Converting ${file}"
     mkdir -p "Assets/Plugins/CandyCoded/${file%/*}"
     pandoc -V geometry:margin=1in -V urlcolor=red "$file" -o "Assets/Plugins/CandyCoded/${file%.md}.pdf"
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/cache" ]; then
 
-    mkdir -p -m 777 "$HOME/cache"
+    mkdir -m 777 "$HOME/cache"
 
 fi
 
@@ -10,7 +10,7 @@ echo "Installing pandoc"
 make docs-tools
 
 URL="https://download.unity3d.com/download_unity/9231f953d9d3/MacEditorInstaller/Unity-2017.4.1f1.pkg"
-FILENAME=`basename "$URL"`
+FILENAME=$(basename "$URL")
 
 if [ ! -f "$HOME/cache/$FILENAME" ]; then
 
