95eb7155f80eee7992100f98f7d04f6f0313f6c3
Scott Doxey <neogeek0101@gmail.com>
2018-06-09 16:34:03 -0400

    Added BillboardTowardsCamera component. (#23)
    * Added BillboardTowardsCamera component.
    
    * Added missing namespace.

---
+++ Assets/Plugins/CandyCoded/Scripts/Components/BillboardTowardsCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace CandyCoded
+{
+
+    public class BillboardTowardsCamera : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Transform mainCamera;
+
+        private void Awake()
+        {
+
+            if (mainCamera == null)
+            {
+
+                mainCamera = Camera.main.transform;
+
+            }
+
+        }
+
+        private void Update()
+        {
+
+            gameObject.transform.LookAt(gameObject.transform.position + mainCamera.rotation * Vector3.forward);
+
+        }
+
+    }
+
+}
