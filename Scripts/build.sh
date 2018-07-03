3c9b8d2e4c35a19f7900df99241a75360888cc35
Scott Doxey <neogeek0101@gmail.com>
2018-07-02 22:45:27 -0400

    Added build tag to build process.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,20 @@
 
 mkdir -p build
 
+CURRENT_TAG=`git describe --tags --abbrev=0`
+CURRENT_TAG_HASH=`git show-ref --hash=7 "${CURRENT_TAG}"`
+CURRENT_HASH=`git rev-parse --short HEAD`
+
+if [ "${CURRENT_TAG_HASH}" = "${CURRENT_HASH}" ]; then
+
+    BUILD_TAG=${CURRENT_TAG}
+
+else
+
+    BUILD_TAG="master"
+
+fi
+
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
     -batchmode \
     -nographics \
@@ -9,5 +23,5 @@ mkdir -p build
     -logFile "$(pwd)/unity.log" \
     -projectPath "$(pwd)/" \
     -exportPackage "Assets/Plugins" \
-    "$(pwd)/build/CandyCoded.unitypackage" \
+    "$(pwd)/build/CandyCoded-${BUILD_TAG}.unitypackage" \
     -quit
