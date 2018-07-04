f8138ca7eec2e13bf6e46c570a277290b0d6b041
Scott Doxey <neogeek0101@gmail.com>
2018-07-04 08:46:57 -0400

    Extended from IList.

--- Assets/Plugins/CandyCoded/Scripts/CustomEditor/ObservableList.cs
+++ Assets/Plugins/CandyCoded/Scripts/CustomEditor/ObservableList.cs
@@ -1,15 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
-public class ObservableList<T> : List<T>
+public class ObservableList<T> : IList<T>
 {
 
     public delegate void EventHandlerWithItem(T item);
+    public delegate void EventHandlerWithItems(T[] items);
     public delegate void EventHandler();
+
     public event EventHandlerWithItem AddEvent;
     public event EventHandler ClearEvent;
     public event EventHandler RemoveEvent;
 
-    public new void Add(T item)
+    private IList<T> _items;
+
+    public int Count
+    {
+        get { return _items.Count; }
+    }
+
+    public bool IsReadOnly
+    {
+        get { return _items.IsReadOnly; }
+    }
+
+    public ObservableList()
+    {
+
+        _items = new List<T>();
+
+    }
+
+    public ObservableList(IList<T> items)
+    {
+
+        _items = new List<T>(items);
+
+    }
+
+    public ObservableList(IEnumerable<T> items)
+    {
+
+        _items = new List<T>(items);
+
+    }
+
+    public T this[int index]
+    {
+        get { return _items[index]; }
+        set { _items[index] = value; }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+
+        return _items.GetEnumerator();
+
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+
+        return GetEnumerator();
+
+    }
+
+    public void Add(T item)
     {
 
         if (AddEvent != null)
@@ -17,11 +73,11 @@ public class ObservableList<T> : List<T>
             AddEvent(item);
         }
 
-        base.Add(item);
+        _items.Add(item);
 
     }
 
-    public new void Clear()
+    public void Clear()
     {
 
         if (ClearEvent != null)
@@ -29,11 +85,44 @@ public class ObservableList<T> : List<T>
             ClearEvent();
         }
 
-        base.Clear();
+        _items.Clear();
 
     }
 
-    public new bool Remove(T item)
+    public bool Contains(T item)
+    {
+
+        return _items.Contains(item);
+
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+
+        _items.CopyTo(array, arrayIndex);
+
+    }
+
+    public int IndexOf(T item)
+    {
+
+        return _items.IndexOf(item);
+
+    }
+
+    public void Insert(int index, T item)
+    {
+
+        if (AddEvent != null)
+        {
+            AddEvent(item);
+        }
+
+        _items.Insert(index, item);
+
+    }
+
+    public bool Remove(T item)
     {
 
         if (RemoveEvent != null)
@@ -41,7 +130,20 @@ public class ObservableList<T> : List<T>
             RemoveEvent();
         }
 
-        return base.Remove(item);
+        return _items.Remove(item);
 
     }
+
+    public void RemoveAt(int index)
+    {
+
+        if (RemoveEvent != null)
+        {
+            RemoveEvent();
+        }
+
+        _items.RemoveAt(index);
+
+    }
+
 }
