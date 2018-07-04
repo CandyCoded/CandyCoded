920cddffe1289dc135f1455a8b9c3a9998625e4e
Scott Doxey <neogeek0101@gmail.com>
2018-07-04 08:53:50 -0400

    Added ObservableListReference.

---
+++ Assets/Plugins/CandyCoded/ScriptableObjects/ObservableListReference.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+    public abstract class ObservableListReference<T> : ListReference<T>
+    {
+
+        [SerializeField]
+        private ObservableList<T> _items = new ObservableList<T>();
+        public new ObservableList<T> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+    }
+
+}
