76d116b9302a8e610444ae95a2b9d0fb811988b7
Scott Doxey <neogeek0101@gmail.com>
2018-07-03 21:27:46 -0400

    Added ObservableList class.

---
+++ Assets/Plugins/CandyCoded/Scripts/CustomEditor/ObservableList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public class ObservableList<T> : List<T>
+{
+
+    public delegate void EventHandlerWithItem(T item);
+    public delegate void EventHandler();
+    public event EventHandlerWithItem AddEvent;
+    public event EventHandler ClearEvent;
+    public event EventHandler RemoveEvent;
+
+    public new void Add(T item)
+    {
+
+        if (AddEvent != null)
+        {
+            AddEvent(item);
+        }
+
+        base.Add(item);
+
+    }
+
+    public new void Clear()
+    {
+
+        if (ClearEvent != null)
+        {
+            ClearEvent();
+        }
+
+        base.Clear();
+
+    }
+
+    public new bool Remove(T item)
+    {
+
+        if (RemoveEvent != null)
+        {
+            RemoveEvent();
+        }
+
+        return base.Remove(item);
+
+    }
+}
