4f2d65f972b81997b0ec6b279926775df85d4bfa
Scott Doxey <neogeek0101@gmail.com>
2018-07-05 22:34:04 -0400

    Added missing methods to ObservableListReference SO.

--- Assets/Plugins/CandyCoded/ScriptableObjects/ObservableListReference.cs
+++ Assets/Plugins/CandyCoded/ScriptableObjects/ObservableListReference.cs
@@ -1,18 +1,73 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CandyCoded
 {
 
-    public abstract class ObservableListReference<T> : ListReference<T>
+    public abstract class ObservableListReference<T> : CustomScriptableObject
     {
 
         [SerializeField]
         private ObservableList<T> _items = new ObservableList<T>();
-        public new ObservableList<T> Items
+        public ObservableList<T> Items
         {
             get { return _items; }
             set { _items = value; }
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        /// <summary>
+        /// Adds an item to the list attached to the scriptable object.
+        /// </summary>
+        /// <param name="item">Item to add to the list.</param>
+        /// <returns>void</returns>
+        public void Add(T item)
+        {
+
+            if (!_items.Contains(item))
+            {
+
+                _items.Add(item);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Removes an item from the list attached to the scriptable object.
+        /// </summary>
+        /// <param name="item">Item to remove from the list.</param>
+        /// <returns>bool</returns>
+        public bool Remove(T item)
+        {
+
+            return _items.Remove(item);
+
+        }
+
+        /// <summary>
+        /// Clears the list attached to the scriptable object.
+        /// </summary>
+        /// <returns>void</returns>
+        public void Clear()
+        {
+
+            _items.Clear();
+
+        }
+
+        /// <summary>
+        /// Clears the list attached to the scriptable object.
+        /// </summary>
+        /// <returns>void</returns>
+        public override void Reset()
+        {
+
+            Clear();
+
         }
 
     }
