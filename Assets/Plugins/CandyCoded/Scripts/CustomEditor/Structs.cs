0e885de4a2cd5d915fd2241f330d4143370a6a85
Scott Doxey <neogeek0101@gmail.com>
2018-08-02 23:34:33 -0400

    Fixed issue "Track lack of copyright and license headers"

--- Assets/Plugins/CandyCoded/Scripts/CustomEditor/Structs.cs
+++ Assets/Plugins/CandyCoded/Scripts/CustomEditor/Structs.cs
@@ -1,3 +1,27 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Scott Doxey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 using System;
 using UnityEngine;
 
@@ -34,7 +58,7 @@ namespace CandyCoded
             return new Vector2AnimationCurve
             {
                 x = new AnimationCurve(x.keys),
-                y = new AnimationCurve(y.keys)
+                    y = new AnimationCurve(y.keys)
             };
 
         }
@@ -139,8 +163,8 @@ namespace CandyCoded
             return new Vector3AnimationCurve
             {
                 x = new AnimationCurve(x.keys),
-                y = new AnimationCurve(y.keys),
-                z = new AnimationCurve(z.keys)
+                    y = new AnimationCurve(y.keys),
+                    z = new AnimationCurve(z.keys)
             };
 
         }
@@ -255,9 +279,9 @@ namespace CandyCoded
             return new Vector4AnimationCurve
             {
                 x = new AnimationCurve(x.keys),
-                y = new AnimationCurve(y.keys),
-                z = new AnimationCurve(z.keys),
-                w = new AnimationCurve(w.keys)
+                    y = new AnimationCurve(y.keys),
+                    z = new AnimationCurve(z.keys),
+                    w = new AnimationCurve(w.keys)
             };
 
         }
