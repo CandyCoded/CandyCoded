4d82cba5143fd2fcd1c2cfc3881098af9a9d7725
Scott Doxey <neogeek0101@gmail.com>
2019-02-03 23:02:35 -0500

    Prevent Tests folder from being added to Unity package build. [skip ci]

--- Scripts/build_package.sh
+++ Scripts/build_package.sh
@@ -6,6 +6,8 @@ CURRENT_TAG=$(git describe --tags --abbrev=0)
 CURRENT_TAG_HASH=$(git show-ref --hash=7 "${CURRENT_TAG}")
 CURRENT_HASH=$(git rev-parse --short HEAD)
 
+mv Assets/Plugins/CandyCoded/Tests Assets/Plugins/CandyCoded/Tests~
+
 BUILD_TAG=$(cat Documentation/Version.txt)
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -17,3 +19,5 @@ BUILD_TAG=$(cat Documentation/Version.txt)
     -exportPackage "Assets/Plugins" \
     "$(pwd)/build/CandyCoded-${BUILD_TAG}.unitypackage" \
     -quit
+
+mv Assets/Plugins/CandyCoded/Tests~ Assets/Plugins/CandyCoded/Tests
