0575053885efcb86d8bd34a9283ab068915c5431
Scott Doxey <neogeek0101@gmail.com>
2019-02-03 22:48:37 -0500

    Updated build tag variable. [skip ci]

--- Scripts/build_package.sh
+++ Scripts/build_package.sh
@@ -6,15 +6,7 @@ CURRENT_TAG=$(git describe --tags --abbrev=0)
 CURRENT_TAG_HASH=$(git show-ref --hash=7 "${CURRENT_TAG}")
 CURRENT_HASH=$(git rev-parse --short HEAD)
 
-if [ "${CURRENT_TAG_HASH}" = "${CURRENT_HASH}" ]; then
-
-    BUILD_TAG=${CURRENT_TAG}
-
-else
-
-    BUILD_TAG="master"
-
-fi
+BUILD_TAG=$(cat Documentation/Version.txt)
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
     -batchmode \
