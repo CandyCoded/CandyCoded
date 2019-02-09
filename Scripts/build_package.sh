cd1ca3b80a04b065fb43c7f271a4fd2b81439723
Scott Doxey <neogeek0101@gmail.com>
2019-02-09 18:11:17 -0500

    Removed unused variables.

--- Scripts/build_package.sh
+++ Scripts/build_package.sh
@@ -2,10 +2,6 @@
 
 mkdir -p build
 
-CURRENT_TAG=$(git describe --tags --abbrev=0)
-CURRENT_TAG_HASH=$(git show-ref --hash=7 "${CURRENT_TAG}")
-CURRENT_HASH=$(git rev-parse --short HEAD)
-
 mv Assets/Plugins/CandyCoded/Tests Assets/Plugins/CandyCoded/Tests~
 
 BUILD_TAG=$(cat Documentation/Version.txt)
