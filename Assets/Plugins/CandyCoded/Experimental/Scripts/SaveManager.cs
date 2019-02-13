372228c72e45d70ad94c81c91a088c66102fdb1a
Scott Doxey <neogeek0101@gmail.com>
2019-02-13 00:28:32 -0500

    Added new experimental class SaveManager.

---
+++ Assets/Plugins/CandyCoded/Experimental/Scripts/SaveManager.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CandyCoded.Experimental
+{
+
+    public static class SaveManager
+    {
+
+        public static bool SaveData<T>(T obj, string filePath)
+        {
+
+            var bf = new BinaryFormatter();
+
+            var fs = new FileStream(filePath, FileMode.Create);
+
+            bf.Serialize(fs, obj);
+
+            fs.Close();
+
+            return true;
+
+        }
+
+        public static T LoadData<T>(string filePath)
+        {
+
+            if (File.Exists(filePath))
+            {
+
+                var bf = new BinaryFormatter();
+
+                var fs = new FileStream(filePath, FileMode.Open);
+
+                var data = (T)bf.Deserialize(fs);
+
+                fs.Close();
+
+                return data;
+
+            }
+
+            throw new FileNotFoundException("File not found.");
+
+        }
+
+    }
+
+}
