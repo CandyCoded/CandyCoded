6d3f94065d90b47b0f23df23c42723756f2335bd
Scott Doxey <neogeek0101@gmail.com>
2019-02-21 21:27:15 -0500

    New experimental feature SaveManager (#81)

---
+++ Assets/Plugins/CandyCoded/Tests/Editor/SaveManagerTest.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using CandyCoded;
+using CandyCoded.Experimental;
+
+public class SaveManagerTest : TestSetup
+{
+
+    [SetUp]
+    public static void DeleteFiles()
+    {
+
+        FileUtil.DeleteFileOrDirectory(Application.persistentDataPath + Path.DirectorySeparatorChar + "List.dat");
+        FileUtil.DeleteFileOrDirectory(Application.persistentDataPath + Path.DirectorySeparatorChar + "ObservableList.dat");
+
+    }
+
+    [Test]
+    public void SaveAndLoadList()
+    {
+
+        var listOfNumbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        SaveManager.SaveData(listOfNumbers, Application.persistentDataPath + Path.DirectorySeparatorChar + "List.dat");
+
+        var newListOfNumbers = SaveManager.LoadData<List<int>>(Application.persistentDataPath + Path.DirectorySeparatorChar + "List.dat");
+
+        Assert.AreEqual(listOfNumbers, newListOfNumbers);
+
+    }
+
+    [Test]
+    public void SaveAndLoadObservableList()
+    {
+
+        var listOfNumbers = new ObservableList<int> { 1, 2, 3, 4, 5 };
+
+        SaveManager.SaveData(listOfNumbers, Application.persistentDataPath + Path.DirectorySeparatorChar + "ObservableList.dat");
+
+        var newListOfNumbers = SaveManager.LoadData<ObservableList<int>>(Application.persistentDataPath + Path.DirectorySeparatorChar + "ObservableList.dat");
+
+        Assert.AreEqual(listOfNumbers, newListOfNumbers);
+
+    }
+
+}
+#endif
