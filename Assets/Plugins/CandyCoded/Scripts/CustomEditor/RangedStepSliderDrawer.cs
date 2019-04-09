2c51be6dc0bfe5fdeaac848379189ec3574d19fd
Scott Doxey <neogeek0101@gmail.com>
2019-04-09 18:59:17 -0400

    Added RangedStepSlider property drawer.

---
+++ Assets/Plugins/CandyCoded/Scripts/CustomEditor/RangedStepSliderDrawer.cs
@@ -0,0 +1,56 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(RangedFloat))]
+#endif
+    public class RangedStepSliderAttribute : PropertyAttribute
+    {
+
+        public float minLimit;
+        public float maxLimit;
+        public float stepIncrement;
+
+        public RangedStepSliderAttribute(float min, float max, float step)
+        {
+            minLimit = min;
+            maxLimit = max;
+            stepIncrement = step;
+        }
+
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(RangedStepSliderAttribute))]
+    public class RangedStepSliderDrawer : PropertyDrawer
+    {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+
+            var limits = attribute as RangedStepSliderAttribute;
+
+            var minValue = property.FindPropertyRelative("min").floatValue;
+            var maxValue = property.FindPropertyRelative("max").floatValue;
+
+            label.tooltip = string.Format("[{0}, {1}]", minValue, maxValue);
+
+            EditorGUI.MinMaxSlider(position, label, ref minValue, ref maxValue, limits.minLimit, limits.maxLimit);
+
+            minValue = Mathf.Round(minValue / limits.stepIncrement) * limits.stepIncrement;
+            maxValue = Mathf.Round(maxValue / limits.stepIncrement) * limits.stepIncrement;
+
+            property.FindPropertyRelative("min").floatValue = minValue;
+            property.FindPropertyRelative("max").floatValue = maxValue;
+
+        }
+
+    }
+#endif
+
+}
