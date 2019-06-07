6006d07555bc6ce3afb44ef9e083878b670ed21e
neogeek <neogeek0101@gmail.com>
2019-06-06 23:40:56 -0400

    Removed unused imports.

--- Assets/Plugins/CandyCoded/Tests/Editor/GameObjectPoolReferenceTest.cs
+++ Assets/Plugins/CandyCoded/Tests/Editor/GameObjectPoolReferenceTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using UnityEngine.TestTools;
 #if UNITY_EDITOR || UNITY_STANDALONE
-using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
 
