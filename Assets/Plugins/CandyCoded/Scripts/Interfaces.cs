c4a252f697985ef3e7f021a370562c9ade5efa01
neogeek <neogeek0101@gmail.com>
2019-07-14 23:25:58 -0400

    Added CandyCoded.Interfaces class.

---
+++ Assets/Plugins/CandyCoded/Scripts/Interfaces.cs
@@ -0,0 +1,22 @@
+namespace CandyCoded.Interfaces
+{
+
+    internal interface IPauseable
+    {
+
+        void Pause();
+
+        void Resume();
+
+    }
+
+    internal interface IToggleable
+    {
+
+        void On();
+
+        void Off();
+
+    }
+
+}
