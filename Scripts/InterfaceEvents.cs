b12e981c0433146d0223831d89721ccce8f13d2a
Scott Doxey <neogeek0101@gmail.com>
2019-07-15 00:00:26 -0400

    Added CandyCoded.Interfaces class. (#110)
    * Added CandyCoded.Interfaces class.
    
    * Added InterfaceEvents static script.
    
    * Whitespace.

---
+++ Scripts/InterfaceEvents.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using UnityEngine;
+
+namespace CandyCoded.Interfaces
+{
+
+    public static class InterfaceEvents
+    {
+
+        public static void PauseAllPausableObjects()
+        {
+
+            var pausableObjects = Object.FindObjectsOfType<MonoBehaviour>().OfType<IPauseable>();
+
+            foreach (var pausableObject in pausableObjects)
+            {
+
+                pausableObject.Pause();
+
+            }
+
+        }
+
+        public static void ResumeAllPausableObjects()
+        {
+
+            var pausableObjects = Object.FindObjectsOfType<MonoBehaviour>().OfType<IPauseable>();
+
+            foreach (var pausableObject in pausableObjects)
+            {
+
+                pausableObject.Resume();
+
+            }
+
+        }
+
+        public static void TurnOffAllToggleableObjects()
+        {
+
+            var toggleableObjects = Object.FindObjectsOfType<MonoBehaviour>().OfType<IToggleable>();
+
+            foreach (var toggleableObject in toggleableObjects)
+            {
+
+                toggleableObject.Off();
+
+            }
+
+        }
+
+        public static void TurnOnAllToggleableObjects()
+        {
+
+            var toggleableObjects = Object.FindObjectsOfType<MonoBehaviour>().OfType<IToggleable>();
+
+            foreach (var toggleableObject in toggleableObjects)
+            {
+
+                toggleableObject.On();
+
+            }
+
+        }
+
+    }
+
+}
