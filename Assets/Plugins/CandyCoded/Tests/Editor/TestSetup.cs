027b893f889d3111fd133a8c657c8100cb22accf
neogeek <neogeek0101@gmail.com>
2019-08-24 14:39:10 -0400

    Added missing test conditionals.

--- Assets/Plugins/CandyCoded/Tests/Editor/TestSetup.cs
+++ Assets/Plugins/CandyCoded/Tests/Editor/TestSetup.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
+#if UNITY_EDITOR || UNITY_STANDALONE
 using NUnit.Framework;
 using UnityEditor.SceneManagement;
 
@@ -25,3 +26,4 @@ namespace CandyCoded.Tests
     }
 
 }
+#endif
