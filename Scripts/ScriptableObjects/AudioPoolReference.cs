d3a4883e2f8d5ad2e0080732f1133042834c35b3
Scott Doxey <neogeek0101@gmail.com>
2020-06-19 00:47:13 -0400

    Added ReSharper disable comment.

--- Scripts/ScriptableObjects/AudioPoolReference.cs
+++ Scripts/ScriptableObjects/AudioPoolReference.cs
@@ -1,5 +1,7 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
+// ReSharper disable RequiredBaseTypesConflict
+
 using System;
 using System.Collections;
 using UnityEngine;
