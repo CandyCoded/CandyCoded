f0a24c62544499e99555f805776bc18dd09ce1d8
Scott Doxey <neogeek0101@gmail.com>
2020-06-19 00:46:23 -0400

    Added missing comment header.

--- Scripts/ScriptableObjects/PoolReference.cs
+++ Scripts/ScriptableObjects/PoolReference.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
