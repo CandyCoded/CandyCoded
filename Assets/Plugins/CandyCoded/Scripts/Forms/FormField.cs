7b791d443139c38623702fea62fb2d53aac81bc2
Scott Doxey <neogeek0101@gmail.com>
2020-07-09 02:36:32 -0400

    Retrieve values from input component if available.

--- Assets/Plugins/CandyCoded/Scripts/Forms/FormField.cs
+++ Assets/Plugins/CandyCoded/Scripts/Forms/FormField.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CandyCoded.Experimental
 {
@@ -33,33 +34,71 @@ namespace CandyCoded.Experimental
             }
         }
 
-        public object value { get; internal set; }
+        private object _value;
+
+        public object value
+        {
+            get
+            {
+
+                if (gameObject.TryGetComponent<InputField>(out var inputField))
+                {
+
+                    return inputField.text;
+
+                }
+
+                if (gameObject.TryGetComponent<Toggle>(out var toggle))
+                {
+
+                    return toggle.isOn;
+
+                }
+
+                if (gameObject.TryGetComponent<Dropdown>(out var dropdown))
+                {
+
+                    return dropdown.value;
+
+                }
+
+                if (gameObject.TryGetComponent<Slider>(out var slider))
+                {
+
+                    return slider.value;
+
+                }
+
+                return _value;
+
+            }
+        }
 
         public void SetStringValue(string value)
         {
 
-            this.value = value;
+            _value = value;
 
         }
 
         public void SetIntValue(int value)
         {
 
-            this.value = value;
+            _value = value;
 
         }
 
         public void SetFloatValue(float value)
         {
 
-            this.value = value;
+            _value = value;
 
         }
 
         public void SetBoolValue(bool value)
         {
 
-            this.value = value;
+            _value = value;
 
         }
 
