f97f76fe4ae0801eaed1465c232e223a718ece0e
Scott Doxey <neogeek0101@gmail.com>
2021-03-26 01:34:23 -0400

    Removed build tag from DLL.

--- Scripts/build_dll.sh
+++ Scripts/build_dll.sh
@@ -2,7 +2,5 @@
 
 mkdir -p Build
 
-BUILD_TAG=$(cat Documentation/Version.txt)
-
 dotnet build CandyCoded.sln
-cp Temp/bin/Debug/CandyCoded.dll "Build/CandyCoded-${BUILD_TAG}.dll"
+cp Temp/bin/Debug/CandyCoded.dll "Build/CandyCoded.dll"
